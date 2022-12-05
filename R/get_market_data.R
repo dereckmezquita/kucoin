fa17abe4348b3f037277c03c9c6e31b5c19b9768
dereckdemezquita <dereck@demezquita.com>
2022-12-04 22:48:25 -0500

    Clarify example.

--- R/get_market_data.R
+++ R/get_market_data.R
@@ -32,7 +32,7 @@
 #'
 #' @examples
 #'
-#' # get one pair of symbol prices
+#' # get one symbol
 #' kucoin::get_market_data(
 #'     symbols = "BTC/USDT",
 #'     from = "2022-11-05 00:00:00",
@@ -40,7 +40,7 @@
 #'     frequency = "1 hour"
 #' )
 #' 
-#' # get multiple pair of symbols prices
+#' # get multiple symbols
 #' kucoin::get_market_data(
 #'     symbols = c("BTC/USDT", "XMR/BTC", "KCS/USDT"),
 #'     from = "2022-11-05 00:00:00",
