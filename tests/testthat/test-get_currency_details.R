eefa564336566c0ec6d04cb7de4a277582a950b4
Dereck Mezquita <dereck@mezquita.io>
2024-08-03 20:25:06 -0500

    Fixed tests.

--- tests/testthat/test-get_currency_details.R
+++ tests/testthat/test-get_currency_details.R
@@ -1,6 +1,16 @@
+box::use(kucoin[ get_currency_details ])
+box::use(testthat[ expect_equal, test_that ])
 
-currencies <- kucoin::get_currency_details("BTC")
+currencies <- get_currency_details("BTC")
 
-testthat::test_that("all columns from currency data returned completely", {
-    testthat::expect_equal(colnames(currencies), c("currency", "name", "full_name", "precision", "is_margin_enabled", "is_debit_enabled", "chain_name", "chain", "withdrawal_min_size", "withdrawal_min_fee", "is_withdraw_enabled", "is_deposit_enabled", "confirms", "pre_confirms", "contract_address"))
+test_that("all columns from currency data returned completely", {
+    expected_columns <- c(
+        "currency", "name", "full_name", "precision", "is_margin_enabled",
+        "is_debit_enabled", "chain_name", "chain", "withdrawal_min_size",
+        "deposit_min_size", "withdraw_fee_rate", "withdraw_max_fee", 
+        "withdrawal_min_fee", "is_withdraw_enabled", "is_deposit_enabled",
+        "confirms", "pre_confirms", "contract_address"
+    )
+
+    expect_equal(colnames(currencies), expected_columns)
 })
--- tests/testthat/test-get_market_data.R
+++ tests/testthat/test-get_market_data.R
@@ -1,18 +1,8 @@
+box::use(kucoin[ get_market_data ])
+box::use(testthat[ expect_equal, test_that ])
+
 # get one pair of symbol prices
-prices <- kucoin::get_market_data(
-    symbols = "BTC/USDT",
-    from = "2022-11-05 00:00:00",
-    to = "2022-11-06 00:00:00",
-    frequency = "1 hour"
-)
-
-# test that all columns from historical data returned completely
-testthat::test_that("all columns from historical data completed", {
-    testthat::expect_equal(colnames(prices), c("symbol", "datetime", "open", "high", "low", "close", "volume", "turnover"))
-})
-
-# get multiple pair of symbol prices
-prices <- kucoin::get_market_data(
+prices <- get_market_data(
     symbols = c("BTC/USDT", "XMR/BTC", "KCS/BTC"),
     from = "2022-11-05 00:00:00",
     to = "2022-11-06 00:00:00",
@@ -20,6 +10,9 @@ prices <- kucoin::get_market_data(
 )
 
 # test that all columns from historical data returned completely
-testthat::test_that("all columns from historical data completed", {
-    testthat::expect_equal(colnames(prices), c("symbol", "datetime", "open", "high", "low", "close", "volume", "turnover"))
+test_that("all columns from historical data completed", {
+    expect_equal(
+        colnames(prices),
+        c("symbol", "datetime", "open", "high", "low", "close", "volume", "turnover")
+    )
 })
