8180d074b01aad58279ad74ea55654f78d33d940
Dereck Mezquita <dereck@mezquita.io>
2025-02-22 15:04:15 -0600

    Working getting api keys info.

--- scripts/tests-manual/run-single_test.R
+++ scripts/tests-manual/run-single_test.R
@@ -2,7 +2,7 @@ if (interactive()) setwd("./scripts/tests-manual")
 
 box::use(
     ../../R/impl_account_account_and_funding[
-        get_account_summary_info_impl
+        get_apikey_info_impl
     ],
     ../../R/utils[get_api_keys, get_base_url],
     coro[async, await],
@@ -14,10 +14,10 @@ main_async <- async(function() {
     keys <- get_api_keys()
     base_url <- get_base_url()
 
-    # 1. Retrieve account summary information
-    account_summary <- await(get_account_summary_info_impl(keys = keys, base_url = base_url))
-    cat("\nAccount Summary:\n")
-    print(account_summary)
+    # 2. Retrieve API key information
+    apikey_info <- await(get_apikey_info_impl(keys = keys, base_url = base_url))
+    cat("\nAPI Key Info:\n")
+    print(apikey_info)
 })
 
 # Run the main async function
