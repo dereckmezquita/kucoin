0b5aa006d51f9c61ff5bfa10d7fe301128c21f75
Dereck Mezquita <dereck@mezquita.io>
2025-02-27 11:48:25 -0600

    Draft of adding order function and its tests.

--- scripts/tests-manual/run-impl_spottrading_orders_add_order.R
+++ scripts/tests-manual/run-impl_spottrading_orders_add_order.R
@@ -1,4 +1,5 @@
 # File: scripts/tests-manual/run-impl_spottrading_market_data.R
+if (interactive()) setwd("./scripts/tests-manual")
 
 box::use(
     ../../R/impl_spottrading_orders_add_order[
@@ -6,10 +7,18 @@ box::use(
         add_order_test_impl,
         add_order_batch_impl
     ],
+    ../../R/impl_spottrading_orders_cancel_order[
+        cancel_order_by_order_id_impl,
+        cancel_order_by_client_oid_impl,
+        cancel_partial_order_impl,
+        cancel_all_orders_by_symbol_impl,
+        cancel_all_orders_impl
+    ],
     ../../R/utils[get_api_keys, get_base_url],
     coro[async, await],
     later[loop_empty, run_now],
-    lubridate[now, days]
+    lubridate[now, days],
+    uuid[UUIDgenerate]
 )
 
 # Define the asynchronous main function
@@ -18,18 +27,29 @@ main_async <- async(function() {
     keys <- get_api_keys()
     base_url <- get_base_url()
 
+    local_symbol <- "BTC-USDT"
+
     # Test 1: Add a new order
     cat("Test 1: Add a new order\n")
-    order <- await(add_order_impl(
+    order_dt <- await(add_order_impl(
         type = "limit",
-        symbol = "BTC-USDT",
+        symbol = local_symbol,
         side = "buy",
-        price = "1",                # Extremely low price for testing purposes
-        size = 0.0001,            # Small order size
-        clientOid = UUIDgenerate(), # Generate a unique client order ID
+        price = 1,                # Extremely low price for testing purposes
+        size = 0.0001,              # Small order size
         remark = "Test: limit order at absurd price"
     ))
-    print(order)
+    print(order_dt)
+    returned_orderId <- order_dt$orderId
+
+    # Test 2: Cancel the order
+    cat("Test 2: Cancel the porevious order\n")
+    cancelled_orderId <- await(cancel_order_by_order_id_impl(
+        orderId = returned_orderId,
+        symbol = local_symbol
+    ))
+
+    print(cancelled_orderId)
 })
 
 main_async()
